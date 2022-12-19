d8955f705dacc37873218de3704e9174a619e4eb
gurland <stas.gurland@gmail.com>
2022-12-19 23:47:01 +0200

    feat: add tg bot service

--- infra/workers.tf
+++ infra/workers.tf
@@ -4,7 +4,7 @@ resource "cloudflare_workers_kv_namespace" "power_alarm" {
 
 resource "cloudflare_worker_script" "stats_service" {
   name    = "stats"
-  content = file("../stats_service/src/main.js")
+  content = file("../stats_service/dist/main.js")
 
   kv_namespace_binding {
     name         = "POWER_ALARM_KV"
@@ -17,12 +17,22 @@ resource "cloudflare_worker_script" "stats_service" {
   }
 }
 
-#resource "cloudflare_worker_script" "tg_bot_service" {
-#  name    = "tg_bot"
-#  content = file("../stats_service/src/main.js")
-#
-#  kv_namespace_binding {
-#    name         = "POWER_ALARM"
-#    namespace_id = cloudflare_workers_kv_namespace.power_alarm.id
-#  }
-#}
+resource "cloudflare_worker_script" "tg_bot_service" {
+  name    = "tg_bot"
+  content = file("../bot_service/dist/main.js")
+
+  kv_namespace_binding {
+    name         = "POWER_ALARM"
+    namespace_id = cloudflare_workers_kv_namespace.power_alarm.id
+  }
+
+  secret_text_binding {
+    name = "TELEGRAM_BOT_TOKEN"
+    text = var.telegram_bot_token
+  }
+
+  secret_text_binding {
+    name = "BASE_TG_BOT_SERVICE_API_URL"
+    text = var.base_tg_bot_service_api_url
+  }
+}
