4f229afa0153cadef6e65f83dd973f4ae9cb633e
gurland <stas.gurland@gmail.com>
2022-12-19 18:20:01 +0200

    feat: add POST and GET for /stats and KV storing

--- stats_service/src/main.js
+++ stats_service/src/main.js
@@ -1,35 +1,49 @@
+async function handleStatsPost(request) {
+  const reqBody = await request.json();
+  const { status } = reqBody;
 
+  const currentUTCTimestamp = Math.floor(Date.now() / 1000);
+
+  let stats = JSON.parse(await POWER_ALARM_KV.get("stats"));
+  if (stats === null) {
+    stats = [];
+  }
+
+  stats.push([currentUTCTimestamp, status]);
+  await POWER_ALARM_KV.put("stats", JSON.stringify(stats));
+
+  return new Response(JSON.stringify({"message": "success"}));
+}
+
+async function handleStatsGet() {
+  let stats = await POWER_ALARM_KV.get("stats");
+
+  return new Response(stats);
+}
+
+function UnauthorizedResponse() {
+  const resBody = JSON.stringify({"message": "bad auth"});
+  return new Response(resBody, {
+    status: 401,
+  });
+}
 
 addEventListener('fetch', async event => {
-  const { request } = event;
-  const { url, body } = request;
+  const {request} = event;
+  const {url, body} = request;
 
-  // const x = await ();
+  const authHeader = request.headers.get("Authorization");
+  if (authHeader !== "Bearer " + API_SECRET) {
+    return event.respondWith(UnauthorizedResponse());
+  }
 
-  // const data = {
-  //   event: x
-  // };
-  //
-  // const json = JSON.stringify(data, null, 2);
-  // if (url.endsWith("/stats")) {
-  //
-  // }
-
-  // if (request.method === 'POST') {
-  //   return event.respondWith(handleRequest(request));
-  // } else if (request.method === 'GET') {
-  //   return event.respondWith(new Response(`The request was a GET`));
-  // }
-
-  // return event.respondWith(async (request) => {{
-  //   const retBody = JSON.stringify(await request.json());
-  //   return new Response(retBody);
-  // }})
+  if (url.endsWith("/stats")) {
+    if (request.method === 'POST') {
+      return event.respondWith(handleStatsPost(request));
+    } else if (request.method === 'GET') {
+      return event.respondWith(handleStatsGet(request));
+    }
+  }
 
   return event.respondWith(handleRequest(request));
 });
-
-async function handleRequest(request) {
-  const retBody = JSON.stringify(await request.json());
-  return new Response(retBody);
-}
