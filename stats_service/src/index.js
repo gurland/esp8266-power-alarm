ca5491979ac663f14d5270a8ad0ae42866d460fb
gurland <stas.gurland@gmail.com>
2022-12-19 23:48:32 +0200

    fix: move stats handlers into its own subdirectory

--- stats_service/src/index.js
+++ stats_service/src/index.js
@@ -1,25 +1,5 @@
-async function handleStatsPost(request) {
-  const reqBody = await request.json();
-  const { status } = reqBody;
+import {handleStatsGet, handleStatsLatestGet, handleStatsPost} from "./handlers/stats";
 
-  const currentUTCTimestamp = Math.floor(Date.now() / 1000);
-
-  let stats = JSON.parse(await POWER_ALARM_KV.get("stats"));
-  if (stats === null) {
-    stats = [];
-  }
-
-  stats.push([currentUTCTimestamp, status]);
-  await POWER_ALARM_KV.put("stats", JSON.stringify(stats));
-
-  return new Response(JSON.stringify({"message": "success"}));
-}
-
-async function handleStatsGet() {
-  let stats = await POWER_ALARM_KV.get("stats");
-
-  return new Response(stats);
-}
 
 function UnauthorizedResponse() {
   const resBody = JSON.stringify({"message": "bad auth"});
@@ -42,6 +22,12 @@ addEventListener('fetch', async event => {
       return event.respondWith(handleStatsPost(request));
     } else if (request.method === 'GET') {
       return event.respondWith(handleStatsGet(request));
+    }
+  }
+
+  if (url.endsWith("/stats/latest")) {
+    if (request.method === 'GET') {
+      return event.respondWith(handleStatsLatestGet());
     }
   }
 
